c40f85e0d54ffde9f398d5e3009a6dfcfd2fbdf4
Chris-1965 <56934142+Chris-1965@users.noreply.github.com>
2022-04-10 12:29:48 +0200

    FEAT: Init

---
+++ speak-transcribe/transcribe.js
@@ -0,0 +1,93 @@
+//"use strict";
+
+const urlParams = new URLSearchParams(window.location.search);
+var deeplkey = urlParams.get('deeplkey');
+var translanguage = urlParams.get('translanguage');
+var spokenlanguage = urlParams.get('spokenlanguage');
+var chkTranslate = urlParams.get('chkTranslate');
+
+if (chkTranslate == "On") {
+  $("div").hide();
+}
+alert(`${deeplkey}, ${translanguage}, ${chkTranslate}, ${spokenlanguage}`);
+
+
+
+var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
+var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
+var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEvent;
+let speechResult = "init";
+
+var areaSubtitle = document.querySelector('.subtitle');
+var areaTranslation = document.querySelector('.translation');
+
+// var btnTest = document.querySelector('button');
+//var txtDebug = document.querySelector('.debug');
+
+async function translateText(text2translate, lang) {
+
+  const url = "https://api-free.deepl.com/v2/translate";
+  const token = "54cfddd9-5296-3646-2c47-6c4cfb56af89:fx";
+
+  //alert(`in fun translateText: ${lang}`);
+  let body = `auth_key=${token}&text=${text2translate}&target_lang=${lang}`;
+
+  let param = {
+    method: 'POST',
+    headers: { "Content-type": "application/x-www-form-urlencoded" },
+    body: body
+  }
+
+  fetch(url, param)
+    .then(response => response.json())
+    .then(json => {
+      // handle success
+      //alert(json.areaTranslations[0].text);
+      areaTranslation.textContent = json.translations[0].text;
+    })
+    .catch((error) => {
+      // handle error
+      alert(error);
+      log(`error ${error}`);
+    });
+}
+
+function testSpeech() {
+  areaSubtitle.textContent = " ";
+  areaTranslation.textContent = " ";
+
+  var recognition = new SpeechRecognition();
+
+  recognition.lang = spokenlanguage.value;
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+  recognition.continuous = true;
+
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    speechResult = event.results[0][0].transcript;
+    areaSubtitle.textContent = speechResult;
+    translateText(speechResult, translanguage.value)
+    recognition.stop();
+  }
+
+  recognition.onerror = function (event) {
+    btnTest.disabled = false;
+    btnTest.textContent = 'Start new test';
+    diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+  }
+
+
+  recognition.onend = function (event) {
+    //Fired when the speech recognition service has disconnected.
+    recognition.start();
+    // txtDebug.textContent = "listening ....";
+    // btnTest.textContent = 'Test in progress';
+    console.log('SpeechRecognition.onend');
+  }
+
+}
+
+testSpeech();
+//btnTest.addEventListener('click', testSpeech);
