6958101d388a9e4a9435f0ab4fe6d4841eb1c3de
cmen-cuas <C.Menard@cuas.at>
2022-04-10 20:35:05 +0200

    MAINT: reorderes and code cleaning

--- transcribe.js
+++ transcribe.js
@@ -1,13 +1,16 @@
 //"use strict";
 
 const urlParams = new URLSearchParams(window.location.search);
-const token = urlParams.get('deeplkey');
-var translanguage = urlParams.get('translanguage');
-var spokenlanguage = urlParams.get('spokenlanguage');
-var chkTranslate = urlParams.get('chkTranslate');
 
-//alert(`${deeplkey}, ${translanguage}, ${chkTranslate}, ${spokenlanguage}`);
-let useTranslate = (chkTranslate === null) ? false : true;
+var token = urlParams.get('deeplkey');
+var spokenlanguage = urlParams.get('spokenlanguage');
+var translanguage = urlParams.get('translanguage');
+var transSelected = urlParams.get('transSelected');
+//alert(`${token}, ${translanguage}, ${transSelected}, ${spokenlanguage}`);
+
+let useTranslate = (transSelected === "on") ? true : false;
+
+//alert(`${token}, ${translanguage}, ${transSelected}, ${spokenlanguage}`);
 
 var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition;
 var SpeechGrammarList = SpeechGrammarList || webkitSpeechGrammarList;
@@ -15,18 +18,56 @@ var SpeechRecognitionEvent = SpeechRecognitionEvent || webkitSpeechRecognitionEv
 let speechResult = "init";
 
 var areaSubtitle = document.querySelector('.subtitle');
-var areaTranslation = document.querySelector('.translation');
+//var areaTranslation = document.querySelector('.translation');
 
 // var btnTest = document.querySelector('button');
 //var txtDebug = document.querySelector('.debug');
 
-async function translateText(text2translate, lang) {
+testSpeech(spokenlanguage,useTranslate,translanguage);
+
+function testSpeech(spokenlanguage, useTranslate = false, translanguage="EN") {
+  areaSubtitle.textContent = " ";
+
+  var recognition = new SpeechRecognition();
+
+  recognition.lang = spokenlanguage;
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+  recognition.continuous = true;
+
+  recognition.start();
+
+  recognition.onresult = function (event) {
+    speechResult = event.results[0][0].transcript;
+    if (useTranslate) {
+      translateText(speechResult, translanguage);
+    }
+    else
+      areaSubtitle.textContent = speechResult;
+
+    recognition.stop();
+  }
+
+  recognition.onerror = function (event) {
+    btnTest.disabled = false;
+    btnTest.textContent = 'Start new test';
+    diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
+  }
+
+  recognition.onend = function (event) {
+    //Fired when the speech recognition service has disconnected.
+    recognition.start();
+    console.log('SpeechRecognition.onend');
+  }
+
+}
+
+async function translateText(text2translate, translanguage) {
 
   const url = "https://api-free.deepl.com/v2/translate";
-  const token = token; //"54cfddd9-5296-3646-2c47-6c4cfb56af89:fx";
+  const token = "54cfddd9-5296-3646-2c47-6c4cfb56af89:fx";
 
-  //alert(`in fun translateText: ${lang}`);
-  let body = `auth_key=${token}&text=${text2translate}&target_lang=${lang}`;
+  let body = `auth_key=${token}&text=${text2translate}&target_lang=${translanguage}`;
 
   let param = {
     method: 'POST',
@@ -46,46 +87,5 @@ async function translateText(text2translate, lang) {
     });
 }
 
-function testSpeech(useTranslate) {
-  areaSubtitle.textContent = " ";
-  areaTranslation.textContent = " ";
 
-  var recognition = new SpeechRecognition();
-
-  recognition.lang = spokenlanguage.value;
-  recognition.interimResults = false;
-  recognition.maxAlternatives = 1;
-  recognition.continuous = true;
-
-  recognition.start();
-
-  recognition.onresult = function (event) {
-    speechResult = event.results[0][0].transcript;
-
-    if (useTranslate)
-      var rest = translateText(speechResult, translanguage.value);
-    else
-      areaSubtitle.textContent = speechResult;
-
-    recognition.stop();
-  }
-
-  recognition.onerror = function (event) {
-    btnTest.disabled = false;
-    btnTest.textContent = 'Start new test';
-    diagnosticPara.textContent = 'Error occurred in recognition: ' + event.error;
-  }
-
-
-  recognition.onend = function (event) {
-    //Fired when the speech recognition service has disconnected.
-    recognition.start();
-    // txtDebug.textContent = "listening ....";
-    // btnTest.textContent = 'Test in progress';
-    console.log('SpeechRecognition.onend');
-  }
-
-}
-
-testSpeech(useTranslate);
 //btnTest.addEventListener('click', testSpeech);
